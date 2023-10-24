88854b4f67a07b5a8a872e075d553330b1e2e175
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-10-23 21:23:02 -0300

    configure: docker e dependencias

--- src/main/java/br/com/devkazu/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/devkazu/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,6 +2,10 @@ package br.com.devkazu.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.devkazu.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,11 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController {
   
 
-  @PostMapping(value="path")
-  public void create() {
-      //TODO: process POST request
+  @PostMapping(value="/")
+  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
       
-     
+     System.out.println(candidateEntity);
   }
   
 }
